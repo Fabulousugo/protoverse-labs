2ee18e6ff418346875dfa7de32d69977108fa515
Ugochukwu Onah <Fabulousonah@gmail.com>
2026-01-09 23:18:30 +0000

    changing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto, Space_Grotesk, Space_Mono,Orbitron,Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
@@ -7,6 +7,26 @@ import Footer from "@/components/layout/Footer";
 const inter = Inter({ 
   subsets: ["latin"],
   variable: '--font-body',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: '--font-heading',
+});
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  variable: '--font-logo',
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: '--font-roboto',
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: '--font-montserrat',
 });
 
 export const metadata: Metadata = {
@@ -22,7 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
+      <body className={`${inter.variable} ${spaceGrotesk.variable} ${orbitron.variable} ${roboto.variable} ${montserrat.variable} antialiased`}>
         <Navbar />
         <div className="pt-16">
           {children}
